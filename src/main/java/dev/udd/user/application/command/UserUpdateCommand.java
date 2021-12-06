4a29075d000918a9d33ec129d99ed94818bbfab9
Nicolás Gamboa <nicolasgamboa.a@gmail.com>
2021-12-06 15:43:16 -0300

    agrego feature actualizar usuario

---
+++ src/main/java/dev/udd/user/application/command/UserUpdateCommand.java
@@ -0,0 +1,49 @@
+package dev.udd.user.application.command;
+
+final public class UserUpdateCommand {
+
+    private String name;
+    private String username;
+    private String uuid;
+    private String password;
+    private String email;
+    public UserUpdateCommand(String name, String username, String uuid, String password, String email) {
+
+        this.name = name;
+        this.username = username;
+        this.uuid = uuid;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public String getUuid() {
+
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+
+        this.uuid = uuid;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+}
